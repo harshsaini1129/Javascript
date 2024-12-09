e9266ea5ad2e33e1315007c201135f048aa8bf43
Harsh Saini <harshsaini1129@gmail.com>
2024-12-09 23:22:26 +0530

    conversion

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -1,20 +1,32 @@
 // convert in number//
-let score="35abc"
-console.log(typeof score);
+// let score="35abc"
+// console.log(typeof score);
 
-let valueinnumber= Number(score)
+// let valueinnumber= Number(score)
 
-console.log(typeof valueinnumber);
-console.log(valueinnumber);
-
+// console.log(typeof valueinnumber);
+// console.log(valueinnumber);
 
 // 1=>true 0=>false
-// "" empty string =>false 
+// "" empty string =>false
 //"33"=>33
 //"33abc"=>NaN =>not a number
 //true=>1
 
-let isloggedin="harsh"
+// let isloggedin="harsh"
 
-let booleanisloggedin= Boolean(isloggedin)
-console.log( booleanisloggedin);
+// let booleanisloggedin= Boolean(isloggedin)
+// console.log( booleanisloggedin);
+
+//*********************operations**************************
+
+let str1 = "hello";
+let str2 = "harsh";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+//not good operations
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
