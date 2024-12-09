211710648ecf61074af13ceff69defeb6b8576f2
Harsh Saini <harshsaini1129@gmail.com>
2024-12-09 23:22:39 +0530

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,11 @@
+console.log(null >= 0);
+console.log(null == 0);
+console.log(null > 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined <= 0);
+//comparison converts null to a number as 0
+//for comparison use ===
+
+console.log("2" === 2);
