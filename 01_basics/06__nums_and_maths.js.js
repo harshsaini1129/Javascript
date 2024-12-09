52efdbe32f63fb8f4922a68060ed9cc830185be3
Harsh Saini <harshsaini1129@gmail.com>
2024-12-09 23:23:16 +0530

    nums

---
+++ 01_basics/06__nums_and_maths.js.js
@@ -0,0 +1,55 @@
+const score = 400
+// console.log(score);
+//  const name = new type(arguments);
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+
+const othernumber = 123.8966
+// console.log(othernumber.toPrecision(5));
+
+const hundreds =1000000
+
+// console.log(hundreds.toLocaleString());
+
+
+//*************************************MATHS***************************** */
+//we can acess some mayhematical values using maths. 
+console.log(Math);
+
+console.log(Math.abs(-4));
+console.log(Math.abs(4));
+// ROUND AND FLOOR GIVES lowest round off value
+// console.log(Math.round(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.max(4, 5, 6, 4));
+
+// random gives value bw 0 or 1
+console.log(Math.random());
+// if random value is 0.0055434567654 etc. so to aviod again 0 value  we add +1
+ // if i want value bw 0 and 10 so multiply by 10
+console.log((Math.random() * 10) + 1);
+
+const min = 10
+const max = 20
+//formula
+// floor converts decimal to normal
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+  
+
+
+
+
+
+ 
+ 
+
+
+
+
+
+
+
